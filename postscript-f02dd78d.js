6412b5ac760a95ae57fd16576bf7a996254d26d2
taaakaaaa <victorrenanq@gmail.com>
2026-06-20 22:48:25 -0300

    Fix GitHub Pages project paths

---
+++ postscript-f02dd78d.js
@@ -0,0 +1,12 @@
+await Promise.all([
+  import("./static/scripts/script-0-0e41d61b.js"),
+  import("./static/scripts/script-1-ec880d15.js"),
+  import("./static/scripts/script-2-1f9394e6.js"),
+  import("./static/scripts/script-3-cc8afa9d.js"),
+  import("./static/scripts/script-4-7e107d7e.js"),
+  import("./static/scripts/script-5-632b2822.js"),
+  import("./static/scripts/script-6-c9e900a7.js"),
+  import("./static/scripts/script-7-296b30c3.js"),
+  import("./static/scripts/script-8-5f71ef20.js")
+]);
+await import("./static/scripts/script-9-7ca62114.js");
